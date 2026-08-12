108e50923d88fa2d483e290f7c5f8b7c9a6b287f
HannahRichman02 <hannahrichman.art@gmail.com>
2026-08-12 18:30:03 -0500

    Added Homepage Content

---
+++ src/lib/styles/evenmore.ts
@@ -0,0 +1,3 @@
+export function scrollIntoView(hash: string) {
+    location.hash = "#" + hash;
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,126 @@
+<script>
+    import '$lib/styles/contentcard.css'
+</script>
+<div class="Header">
+    <p class="Welcome">WELCOME IN</p>
+    <img src="/Images/Header_Trip.png" class="HeaderTrip" alt="Header Trip">
+    <img src="/Images/Header_Bar.png" class="HeaderBar" alt="Header Bar">
+    <img src="/Images/HR_Header.png" class="NameTitle" alt="Hannah Richman">
+    <a href=#Work_Section class="Work">Work</a>
+    <a href=#About_Section class="About">About</a>
+    <a href=#Contact_Section class="Contact">Contact</a>
+</div>
+<div class="sleepless-night" id="Work_Section">
+    <img src="/Images/Sleepless_Nights_SC.png" class="SNSC" alt="Sleepless_Night">
+    <p class="Sleepless">SLEEPLESS</p>
+    <p class="Night">NIGHT</p>
+    <img src="/Images/Stars_v02.png" class="Stars_Line" alt=Starries>
+    <img src="/Images/Cosmic_v03.png" class="Cosmic_Swirl" alt=Cosmic-Swirl>
+    <img src="/Images/Chevron.png" class="Chevron" alt=Chevron>
+    <img src="/Images/Button_Timer.png" class="ButtonTimer" alt=Timer>
+    <img src="/Images/Button_BedTime.png" class="ButtonBedTime" alt=Bed-Time>
+    <img src="/Images/Button_Moon.png" class="ButtonMoon" alt=Moon>
+    <img src="/Images/Button_Lock.png" class="ButtonLock" alt=Lock>
+    <img src="/Images/Button_Wifi.png" class="ButtonWifi" alt=Wifi>
+    <img src="/Images/Button_Cellular.png" class="ButtonCellular" alt=Cellular>
+    <p class= "SN_Description">After Effects<br>Illustration<br>Insomnia<br>Transition<br>Audio<br>Visuals</p>
+    <img src="/Images/Planet.png" class="Planet" alt=Planet>
+</div>
+<div class="LivingSpaces">
+    <img src="/Images/Living_Spaces_Title.png" class="LS_Title" alt="Living Spaces" >
+    <img src="/Images/Living_Spaces_Light.png" class="LS_Light01" alt="Living Spaces Echo 1">
+    <img src="/Images/Living_Spaces_Light.png" class="LS_Light02" alt="Living Spaces Echo 2">
+    <img src="/Images/Living_Spaces_Light.png" class="LS_Light03" alt="Living Spaces Echo 3">
+    <img src="/Images/Living_Spaces_Light.png" class="LS_Light04" alt="Living Spaces Echo 4">
+    <img src="/Images/Living_Spaces_Light.png" class="LS_Light05" alt="Living Spaces Echo 5">
+    <img src="/Images/Living_Spaces_SC.png" class="LS_SC" alt="Living Spaces Thumbnail">
+    <img src="/Images/Stars_v01.png" class="Stars_01" alt="Stars Contained">
+    <p class="Household">HOUSEHOLD</p>
+    <img src="/Images/Nokia.png" class="Nokia" alt="Nokia">
+    <img src="/Images/Play_Bar.png" class="Play_Bar" alt="Play Bar">
+    <p class="Blender">BLENDER</p>
+    <p class="W">2026</p>
+    <p class="Satisfaction">Satisfaction</p>
+    <p class="Years">Years</p>
+    <figure class="Line-Graph">
+            <li style="--y: 25%; --x: 9%">
+                <div class="data-point" data-values="25"></div>
+                <div class="line-segment" style="--hypotenuse: 13.453624047074; --angle: -48;"></div>
+            </li>
+            <li style="--y: 35%; --x: 18%">
+                <div class="data-point" data-values="35"></div>
+                <div class="line-segment" style="--hypotenuse: 26.570660511173; --angle: -70;"></div>
+            </li>
+            <li style="--y: 60%; --x: 27%">
+                <div class="data-point" data-value="60"></div>
+                <div class="line-segment" style="--hypotenuse: 21.931712199461; --angle: 66;"></div>
+            </li>
+            <li style="--y: 40%; --x: 36%">
+                <div class="data-point" data-value="40"></div>
+                <div class="line-segment" style="--hypotenuse: 17.492855684536; --angle: -59;"></div>
+            </li>
+            <li style="--y: 55%; --x: 45%">
+                <div class="data-point" data-value="55"></div>
+                <div class="line-segment" style="--hypotenuse: 45.891175622335; --angle: 79;"></div>
+            </li>
+            <li style="--y: 10%; --x: 54%">
+                <div class="data-point" data-value="10"></div>
+                <div class="line-segment" style="--hypotenuse: 13.453624047074; --angle: -48;"></div>
+            </li>
+            <li style="--y: 20%; --x: 63%">
+                <div class="data-point" data-value="20"></div>
+                <div class="line-segment" style="--hypotenuse: 55.731499172371; --angle: -80.5;"></div>
+            </li>
+            <li style="--y: 75%; --x: 72%">
+                <div class="data-point" data-value="75"></div>
+                <div class="line-segment" style="--hypotenuse: 26.570660511173; --angle: 70;"></div>
+            </li>
+            <li style="--y: 50%; --x: 81%">
+                <div class="data-point" data-value="50"></div>
+                <div class="line-segment" style="--hypotenuse: 21.931712199461; --angle: 66;"></div>
+            </li>
+            <li style="--y: 30%; --x: 90%">
+                <div class="data-point" data-value="30"></div>
+            </li>
+    </figure>
+      <div class="Test"></div>
+</div>
+<div class="Logo_Mania">
+    <img src="/Images/Ghostly_Rose.png" class="GhostlyRose" alt="Ghostly Rose">
+    <img src="/Images/Bars.png" class="BarLines" alt="BarLines">
+    <p class="Logo">LOGO</p>
+    <p class="Mania">MANIA</p>
+    <img src="/Images/Arrows.png" class="Arrows" alt="Arrows">
+    <img src="/Images/Rectangles.png" class="Rectangles" alt="Rectangles">
+    <img src="/Images/Rectangles_Rotating.png" class="RRectangles" alt="Rotating Rectangles">
+    <img src="/Images/Checkers.png" class="Checkers" alt="Checkers">
+    <img src="/Images/Swirlies.png" class="Swirlies" alt="Swirlies">
+    <img src="/Images/Search_Bar.png" class="SearchBar" alt="Search Bar">
+    <p class="Branding">BRANDING</p>
+    <img src="/Images/Gradient_Bar.png" class="GradientBar" alt="Gradient Bar">
+    <p class=FluffWords>Gamertags.................<br>Lupine...........................<br>Davinci Resolve.......<br>Animation....................<br>Graphic Design.........</p>
+</div>
+<div class="profile">
+    <p class="barcode">ANIMATION-MOTION DESIGN-EDITING-ILLUSTRATION</p>
+    <img src="/Images/Contact_Profile_01.png" class="Profile_Image" alt="Profile">
+    <p class="ContactFN" id="About_Section">HANNAH</p>
+    <p class="ContactLN">RICHMAN</p>
+    <img src="/Images/Star_Line.png" class="StarLine" alt="Star Divider">
+    <p class="Description">SCAD graduate with a passion for anything and<br>everything motion and design. From Arizona<br>and based in Nebraska, I love to create pieces<br>that feel inspired and tell stories.</p>
+    <img src="/Images/Banner.png" class="Banner" alt="Banner">
+    <img src="/Images/Moon_Phases.png" class="Moons" alt="Moon Phases">
+    <img src="/Images/Battery_v01.png" class="Battery01" alt="Battery">
+    <img src="/Images/Black_Hole_v01.png" class="BlackHole" alt="Black Hole Topography">
+    <img src="/Images/Circuits.png" class="Circuits" alt="Circuits">
+    <img src="/Images/Record_v01.png" class="Record" alt="Record">
+    <img src="/Images/Cosmic_v02.png" class="Cosmic" alt="Cosmic Dust">
+    <img src="/Images/Stars.png" class="Stars" alt="Stars">
+    <img src="/Images/Check_Engine_Light.png" class="CEL" alt="Check Engine Light">
+    <img src="/Images/Battery_v02.png" class="Battery02" alt="Battery Light">
+    <img src="/Images/Tire_Pressure.png" class="TirePressure" alt="Tire Pressure Light">
+    <img src="/Images/Airbag.png" class="Airbag" alt="Airbag Light">
+    <div class="XO"><div class="XOX">XOXOXO</div><br><div class="XOXO">OXOXOX</div><br><div class="XOXOX">XOXOXO</div></div>
+    <p class="SCAD">SCAD●2024</p>
+    <p class="Email" id="Contact_Section">hannahrichman.art@gmail.com</p>
+    <p class="Copyright">All work © Hannah Richman 2026</p>
+</div>
