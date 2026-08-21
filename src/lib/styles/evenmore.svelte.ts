bb46a62d7517bf44499974034a94ffa75d521aa2
HannahRichman02 <hannahrichman.art@gmail.com>
2026-08-21 18:29:30 -0500

    Graphs JS

--- src/lib/styles/evenmore.svelte.ts
+++ src/lib/styles/evenmore.svelte.ts
@@ -1,9 +1,11 @@
+import { browser } from '$app/environment';
+
 let gif_bedroom_version: number = $state(0)
 
 function restartGif(which: string) {
     switch(which) {
         case'bedroom':
-            gif_bedroom_version =+ 1
+            gif_bedroom_version += 1
             break
         default:
             console.error('Unknown gif version: ${which}')
@@ -20,4 +22,95 @@ function getGifVersion(which: string): number {
     }
 }
 
-export {getGifVersion, restartGif}
+export {getGifVersion, restartGif, lightScroll}
+
+let workHighlights = $state({
+    first: false,
+    second: false,
+    third: false,
+    fourth: false,
+    fifth: false,
+    sixth: false,
+    seventh: false,
+    eighth: false,
+    ninth: false,
+    tenth: false,
+    eleventh: false,
+    twelfth: false,
+    thirteenth: false,
+    fourteenth: false,
+
+})
+
+let aboutHighlights = $state({
+    first: false,
+    second: false,
+    third: false,
+
+})
+
+let contactHighlights = $state({
+    first: false,
+})
+
+
+async function lightScroll(location: string) {
+    switch(location) {
+        case 'Work':
+            workHighlights.first = true;
+            await setTimeout(() => {workHighlights.second = true;}, 150)
+            await setTimeout(() => {workHighlights.third = true;}, 300)
+            await setTimeout(() => {workHighlights.fourth = true;}, 450)
+            await setTimeout(() => {workHighlights.fifth = true;}, 600)
+            await setTimeout(() => {workHighlights.sixth = true;}, 750)
+            await setTimeout(() => {workHighlights.seventh = true;}, 900)
+            await setTimeout(() => {workHighlights.first = false;}, 1000)
+            await setTimeout(() => {workHighlights.eighth = true;}, 1050)
+            await setTimeout(() => {workHighlights.second = false;}, 1150)
+            await setTimeout(() => {workHighlights.ninth = true;}, 1200)
+            await setTimeout(() => {workHighlights.third = false;}, 1300)
+            await setTimeout(() => {workHighlights.tenth = true;}, 1350)
+            await setTimeout(() => {workHighlights.fourth = false;}, 1450)
+            await setTimeout(() => {workHighlights.eleventh = true;}, 1500)
+            await setTimeout(() => {workHighlights.fifth = false;}, 1600)
+            await setTimeout(() => {workHighlights.twelfth = true;}, 1650)
+            await setTimeout(() => {workHighlights.sixth = false;}, 1750)
+            await setTimeout(() => {workHighlights.thirteenth = true;}, 1800)
+            await setTimeout(() => {workHighlights.seventh = false;}, 1900)
+            await setTimeout(() => {workHighlights.fourteenth = true;}, 1950)
+            await setTimeout(() => {workHighlights.eighth = false;}, 2050)
+            await setTimeout(() => {workHighlights.ninth = false;}, 2200)
+            await setTimeout(() => {workHighlights.tenth = false;}, 2350)
+            await setTimeout(() => {workHighlights.eleventh = false;}, 2500)
+            await setTimeout(() => {workHighlights.twelfth = false;}, 2650)
+            await setTimeout(() => {workHighlights.thirteenth = false;}, 2800)
+            await setTimeout(() => {workHighlights.fourteenth = false;}, 2950)
+            break;
+        case 'About':
+            aboutHighlights.first = true, 300;
+            await setTimeout(() => {aboutHighlights.second = true;}, 450)
+            await setTimeout(() => {aboutHighlights.third = true;}, 600)
+            await setTimeout(() => {aboutHighlights.first = false;}, 1300)
+            await setTimeout(() => {aboutHighlights.second = false;}, 1450)
+            await setTimeout(() => {aboutHighlights.third = false;}, 1600)
+            break;
+        case 'Contact':
+                await setTimeout(() => {contactHighlights.first = true;}, 500)
+                await setTimeout(() => {contactHighlights.first = false;}, 1500)
+        default:
+            console.error(`lightScroll: scrolling to ${location} which is not a known location`)
+    }
+}
+
+export function getLightClass(location: String, which: String) {
+    switch (location) {
+        case 'Work':
+            return workHighlights[`${which}` as keyof typeof workHighlights]
+        case 'About':
+            return aboutHighlights[`${which}` as keyof typeof aboutHighlights]
+        case 'Contact':
+            return contactHighlights[`${which}` as keyof typeof contactHighlights]
+    }
+}
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,120 +1,239 @@
-<script>
+<script lang=es>
     import '$lib/styles/contentcard.css'
-    import { getGifVersion, restartGif } from '$lib/styles/evenmore.svelte.ts';
+    import { getGifVersion, restartGif,  getLightClass, lightScroll } from '$lib/styles/evenmore.svelte.ts';
+    import { onMount } from 'svelte';
+    import Chart from 'chart.js/auto';
+
+    /**
+     * @type {HTMLButtonElement | null}
+     */
+    let contactButton = null;
+
+    onMount (() => {
+        if (contactButton == null) {
+            return
+        }
+        contactButton.addEventListener('click', function(){
+        lightScroll('Contact')
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+        });
+    });})
+    
+    const xValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const yValues = [2, 5, 3, 4, 9, 2, 8, 7, 1, 3, 6, 8, 4];
+
+         /**
+         * @type {HTMLButtonElement | null}
+         */
+    let top = null;
+
+    onMount (() => {
+            if (top == null) {
+               return
+           }
+        new Chart(top, {
+            type: "bar",
+            data: {
+                labels: xValues,
+                datasets: [{
+                   backgroundColor: "rgba(117,107,35,1)",
+                   data: yValues
+               }]
+           },
+        options: {
+            plugins: {
+                legend: {display: false},
+                title: {display: false},
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        display: false,
+                    }
+                },
+                y: {
+                    ticks: {
+                        display: false,
+                    }
+                }
+            }
+        }
+       });
+    });
+
+
+    const zValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const wValues = [2, 5, 3, 4, 9, 2, 8, 7, 1, 3, 6, 8, 4];
+
+         /**
+         * @type {HTMLButtonElement | null}
+         */
+    let bot = null;
+
+    onMount (() => {
+            if (bot == null) {
+               return
+           }
+        new Chart(bot, {
+            type: "bar",
+            data: {
+                labels: zValues,
+                datasets: [{
+                   backgroundColor: "rgba(117,107,35,1)",
+                   data: wValues
+               }]
+           },
+        options: {
+            plugins: {
+                legend: {display: false},
+                title: {display: false},
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        display: false,
+                    }
+                },
+                y: {
+                    ticks: {
+                        display: false,
+                    }
+                }
+            }
+        }
+       });
+    });
+
+    const tValues = [9, 18, 27, 36, 45, 54, 63, 72, 81, 90];
+    const vValues = [25, 35, 60, 40, 55, 10, 20, 75, 50, 30];
+
+         /**
+         * @type {HTMLButtonElement | null}
+         */
+    let line = null;
+
+    onMount (() => {
+        if (line == null) {
+            return
+        }
+        new Chart(line, {
+            type: "line",
+            data: {
+                labels: tValues,
+                datasets: [{
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "rgba(200,104,41,1)",
+                    borderColor: "rgba(200,104,41,1)",
+                    data: vValues,
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {display: false},
+                    title: {display: false},
+                },
+                scales: {
+                    x: {
+                        ticks: {
+                            display: false,
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            display: false,
+                        }
+                    }
+                }
+            }
+        })
+    })
+
+  
 </script>
 <div class="Header">
     <p class="Welcome">WELCOME IN</p>
     <img src="/Images/Header_Trip.png" class="HeaderTrip" alt="Header Trip">
     <img src="/Images/Header_Bar.png" class="HeaderBar" alt="Header Bar">
     <img src="/Images/HR_Header.png" class="NameTitle" alt="Hannah Richman">
-    <a href=#Work_Section class="Work">Work</a>
-    <a href=#About_Section class="About">About</a>
-    <a href=#Contact_Section class="Contact">Contact</a>
+    <a href=#Work_Section class="Work" onmousedown={() => lightScroll("Work")}>Work</a>
+    <a href=#About_Section class="About" onmousedown={() => lightScroll("About")}>About</a>
+    <button class="Contact" bind:this={contactButton}>Contact</button>
+    <canvas bind:this={top} class="BarGraphTop" style="width: 100%;max-width:285px; height:100%;max-height:100px">
+    </canvas>
+    <canvas bind:this={bot} class="BarGraphBottom" style="width: 100%;max-width:285px; height:100%;max-height:100px">
+    </canvas>
 </div>
 <div class="sleepless-night" id="Work_Section">
-    <img src="/Images/Sleepless_Nights_SC.png" class="SNSC" alt="Sleepless_Night">
-    <p class="Sleepless">SLEEPLESS</p>
-    <p class="Night">NIGHT</p>
-    <img src="/Images/Stars_v02.png" class="Stars_Line" alt=Starries>
-    <img src="/Images/Cosmic_v03.png" class="Cosmic_Swirl" alt=Cosmic-Swirl>
-    <img src="/Images/Chevron.png" class="Chevron" alt=Chevron>
-    <img src="/Images/Button_Timer.png" class="ButtonTimer" alt=Timer>
-    <img src="/Images/Button_BedTime.png" class="ButtonBedTime" alt=Bed-Time>
-    <img src="/Images/Button_Moon.png" class="ButtonMoon" alt=Moon>
-    <img src="/Images/Button_Lock.png" class="ButtonLock" alt=Lock>
-    <img src="/Images/Button_Wifi.png" class="ButtonWifi" alt=Wifi>
-    <img src="/Images/Button_Cellular.png" class="ButtonCellular" alt=Cellular>
-    <p class= "SN_Description">After Effects<br>Illustration<br>Insomnia<br>Transition<br>Audio<br>Visuals</p>
-    <img src="/Images/Planet.png" class="Planet" alt=Planet>
+    <img src="/Images/Sleepless_Nights_SC.png" class={`SNSC${getLightClass("Work", "first") ? ` highlightborder` : ``}`} alt="Sleepless_Night">
+    <p class={`Sleepless${getLightClass("Work", "first") ? ` highlight` : ``}`}>SLEEPLESS</p>
+    <p class={`Night${getLightClass("Work", "second") ? ` highlight` : ``}`}>NIGHT</p>
+    <img src="/Images/Stars_v02.png" class={`Stars_Line${getLightClass("Work", "second") ? ' highlightorange' : ``}`} alt=Starries>
+    <img src="/Images/Cosmic_v03.png" class={`Cosmic_Swirl${getLightClass("Work", "second") ? ' highlightorange' : ``}`} alt=Cosmic-Swirl>
+    <img src="/Images/Chevron.png" class={`Chevron${getLightClass("Work", "fourth") ? ' highlightyellow' : ``}`} alt=Chevron>
+    <img src="/Images/Button_Timer.png" class={`ButtonTimer${getLightClass("Work", "third") ? ' highlightred' : ``}`} alt=Timer>
+    <img src="/Images/Button_BedTime.png" class={`ButtonBedTime${getLightClass("Work", "third") ? ' highlightred' : ``}`} alt=Bed-Time>
+    <img src="/Images/Button_Moon.png" class={`ButtonMoon${getLightClass("Work", "third") ? ' highlightred' : ``}`} alt=Moon>
+    <img src="/Images/Button_Lock.png" class={`ButtonLock${getLightClass("Work", "third") ? ' highlightred' : ``}`} alt=Lock>
+    <img src="/Images/Button_Wifi.png" class={`ButtonWifi${getLightClass("Work", "third") ? ' highlightred' : ``}`} alt=Wifi>
+    <img src="/Images/Button_Cellular.png" class={`ButtonCellular${getLightClass("Work", "third") ? ' highlightred' : ``}`} alt=Cellular>
+    <p class= {`SN_Description${getLightClass("Work", "third") ? ` highlight` : ``}`}>After Effects<br>Illustration<br>Insomnia<br>Transition<br>Audio<br>Visuals</p>
+    <img src="/Images/Planet.png" class={`Planet${getLightClass("Work", "third") ? ' highlightred' : ``}`} alt=Planet>
 </div>
 <div class="LivingSpaces">
     <a href="/LivingSpaces">
-        <img src="/Images/Living_Spaces_Title.png" class="LS_Title" alt="Living Spaces" >
+        <img src="/Images/Living_Spaces_Title.png" class={`LS_Title${getLightClass("Work", "fifth") ? ' highlightgreen' : ``}`} alt="Living Spaces" >
     </a>
-    <img src="/Images/Living_Spaces_Light.png" class="LS_Light01" alt="Living Spaces Echo 1">
-    <img src="/Images/Living_Spaces_Light.png" class="LS_Light02" alt="Living Spaces Echo 2">
-    <img src="/Images/Living_Spaces_Light.png" class="LS_Light03" alt="Living Spaces Echo 3">
-    <img src="/Images/Living_Spaces_Light.png" class="LS_Light04" alt="Living Spaces Echo 4">
-    <img src="/Images/Living_Spaces_Light.png" class="LS_Light05" alt="Living Spaces Echo 5">
+    <img src="/Images/Living_Spaces_Light.png" class={`LS_Light01${getLightClass("Work", "fifth") ? ' highlightgreen' : ``}`} alt="Living Spaces Echo 1">
+    <img src="/Images/Living_Spaces_Light.png" class={`LS_Light02${getLightClass("Work", "fifth") ? ' highlightgreen' : ``}`} alt="Living Spaces Echo 2">
+    <img src="/Images/Living_Spaces_Light.png" class={`LS_Light03${getLightClass("Work", "fifth") ? ' highlightgreen' : ``}`} alt="Living Spaces Echo 3">
+    <img src="/Images/Living_Spaces_Light.png" class={`LS_Light04${getLightClass("Work", "fifth") ? ' highlightgreen' : ``}`} alt="Living Spaces Echo 4">
+    <img src="/Images/Living_Spaces_Light.png" class={`LS_Light05${getLightClass("Work", "fifth") ? ' highlightgreen' : ``}`} alt="Living Spaces Echo 5">
     <div class="LS-gif-container">
         {#key getGifVersion('bedroom')}
-            <img src="/Images/Bedroom_GIF_Small.gif" class="LS_GIF" alt="Living Spaces Gif" onmouseover={() => restartGif('bedroom')} onfocus={() => restartGif('bedroom')}/>
+            <img src="/Images/Bedroom_GIF_Small_SLSU.gif" class={`LS_GIF${getLightClass("Work", "fifth") ? ` highlightborder` : ``}`} alt="Living Spaces Gif" onmouseover={() => restartGif('bedroom')} onfocus={() => restartGif('bedroom')}/>
         {/key}
-        <img src="/Images/Living_Spaces_SC.png" class="LS_SC" alt="Living Spaces Thumbnail">
+        <a href="/LivingSpaces">
+            <img src="/Images/Living_Spaces_SC.png" class={`LS_SC${getLightClass("Work", "fifth") ? ` highlightborder` : ``}`} alt="Living Spaces Thumbnail" onmouseover={() => restartGif('bedroom')} onfocus={() => restartGif('bedroom')}>
+        </a>
     </div>
-    <img src="/Images/Stars_v01.png" class="Stars_01" alt="Stars Contained">
-    <p class="Household">HOUSEHOLD</p>
-    <img src="/Images/Nokia.png" class="Nokia" alt="Nokia">
-    <img src="/Images/Play_Bar.png" class="Play_Bar" alt="Play Bar">
-    <p class="Blender">BLENDER</p>
-    <p class="W">2026</p>
-    <p class="Satisfaction">Satisfaction</p>
-    <p class="Years">Years</p>
-    <figure class="Line-Graph">
-            <li style="--y: 25%; --x: 9%">
-                <div class="data-point" data-values="25"></div>
-                <div class="line-segment" style="--hypotenuse: 13.453624047074; --angle: -48;"></div>
-            </li>
-            <li style="--y: 35%; --x: 18%">
-                <div class="data-point" data-values="35"></div>
-                <div class="line-segment" style="--hypotenuse: 26.570660511173; --angle: -70;"></div>
-            </li>
-            <li style="--y: 60%; --x: 27%">
-                <div class="data-point" data-value="60"></div>
-                <div class="line-segment" style="--hypotenuse: 21.931712199461; --angle: 66;"></div>
-            </li>
-            <li style="--y: 40%; --x: 36%">
-                <div class="data-point" data-value="40"></div>
-                <div class="line-segment" style="--hypotenuse: 17.492855684536; --angle: -59;"></div>
-            </li>
-            <li style="--y: 55%; --x: 45%">
-                <div class="data-point" data-value="55"></div>
-                <div class="line-segment" style="--hypotenuse: 45.891175622335; --angle: 79;"></div>
-            </li>
-            <li style="--y: 10%; --x: 54%">
-                <div class="data-point" data-value="10"></div>
-                <div class="line-segment" style="--hypotenuse: 13.453624047074; --angle: -48;"></div>
-            </li>
-            <li style="--y: 20%; --x: 63%">
-                <div class="data-point" data-value="20"></div>
-                <div class="line-segment" style="--hypotenuse: 55.731499172371; --angle: -80.5;"></div>
-            </li>
-            <li style="--y: 75%; --x: 72%">
-                <div class="data-point" data-value="75"></div>
-                <div class="line-segment" style="--hypotenuse: 26.570660511173; --angle: 70;"></div>
-            </li>
-            <li style="--y: 50%; --x: 81%">
-                <div class="data-point" data-value="50"></div>
-                <div class="line-segment" style="--hypotenuse: 21.931712199461; --angle: 66;"></div>
-            </li>
-            <li style="--y: 30%; --x: 90%">
-                <div class="data-point" data-value="30"></div>
-            </li>
+    <img src="/Images/Stars_v01.png" class={`Stars_01${getLightClass("Work", "seventh") ? ' highlightorange' : ``}`} alt="Stars Contained">
+    <p class={`Household${getLightClass("Work", "sixth") ? ` highlight` : ``}`}>HOUSEHOLD</p>
+    <img src="/Images/Nokia.png" class={`Nokia${getLightClass("Work", "ninth") ? ` highlightyellow` : ``}`} alt="Nokia">
+    <img src="/Images/Play_Bar.png" class={`Play_Bar${getLightClass("Work", "seventh") ? ' highlightred' : ``}`} alt="Play Bar">
+    <p class={`Blender${getLightClass("Work", "eighth") ? ' highlightyellowBG' : ``}`}>BLENDER</p>
+    <p class={`W${getLightClass("Work", "ninth") ? ` highlight` : ``}`}>2026</p>
+    <p class={`Satisfaction${getLightClass("Work", "eighth") ? ` highlight` : ``}`}>Satisfaction</p>
+    <p class={`Years${getLightClass("Work", "eighth") ? ` highlight` : ``}`}>Years</p>
+    <figure class={`Line-Graph${getLightClass("Work", "eighth") ? ` highlightLG` : ``}`}>
     </figure>
-      <div class="Test"></div>
+    <canvas bind:this={line} class="LineGraphJS" style="width: 100%;max-width:410px; height:100%;max-height:140px">
+    </canvas>
 </div>
 <div class="Logo_Mania">
-    <img src="/Images/Ghostly_Rose.png" class="GhostlyRose" alt="Ghostly Rose">
-    <img src="/Images/Bars.png" class="BarLines" alt="BarLines">
-    <p class="Logo">LOGO</p>
-    <p class="Mania">MANIA</p>
-    <img src="/Images/Arrows.png" class="Arrows" alt="Arrows">
-    <img src="/Images/Rectangles.png" class="Rectangles" alt="Rectangles">
-    <img src="/Images/Rectangles_Rotating.png" class="RRectangles" alt="Rotating Rectangles">
-    <img src="/Images/Checkers.png" class="Checkers" alt="Checkers">
-    <img src="/Images/Swirlies.png" class="Swirlies" alt="Swirlies">
-    <img src="/Images/Search_Bar.png" class="SearchBar" alt="Search Bar">
-    <p class="Branding">BRANDING</p>
-    <img src="/Images/Gradient_Bar.png" class="GradientBar" alt="Gradient Bar">
+    <img src="/Images/Ghostly_Rose.png" class={`GhostlyRose${getLightClass("Work", "tenth") ? ' highlightborder' : ``}`} alt="Ghostly Rose">
+    <img src="/Images/Bars.png" class={`BarLines${getLightClass("Work", "tenth") ? ` highlightgreen` : ``}`} alt="BarLines">
+    <p class={`Logo${getLightClass("Work", "eleventh") ? ` highlight` : ``}`}>LOGO</p>
+    <p class={`Mania${getLightClass("Work", "eleventh") ? ` highlight` : ``}`}>MANIA</p>
+    <img src="/Images/Arrows.png" class={`Arrows${getLightClass("Work", "twelfth") ? ` highlightorange` : ``}`} alt="Arrows">
+    <img src="/Images/Rectangles.png" class={`Rectangles${getLightClass("Work", "thirteenth") ? ` highlightyellow` : ``}`} alt="Rectangles">
+    <img src="/Images/Rectangles_Rotating.png" class={`RRectangles${getLightClass("Work", "twelfth") ? ` highlightgreen` : ``}`} alt="Rotating Rectangles">
+    <img src="/Images/Checkers.png" class={`Checkers${getLightClass("Work", "thirteenth") ? ` highlightred` : ``}`} alt="Checkers">
+    <img src="/Images/Swirlies.png" class={`Swirlies${getLightClass("Work", "twelfth") ? ` highlightyellow` : ``}`} alt="Swirlies">
+    <img src="/Images/Search_Bar.png" class={`SearchBar${getLightClass("Work", "thirteenth") ? ` highlightorange` : ``}`} alt="Search Bar">
+    <p class={`Branding${getLightClass("Work", "thirteenth") ? ` highlight` : ``}`}>BRANDING</p>
+    <img src="/Images/Gradient_Bar.png" class={`GradientBar${getLightClass("Work", "fourteenth") ? ` highlightgreen` : ``}`} alt="Gradient Bar">
     <p class=FluffWords>Gamertags.................<br>Lupine...........................<br>Davinci Resolve.......<br>Animation....................<br>Graphic Design.........</p>
 </div>
 <div class="profile">
     <p class="barcode">ANIMATION-MOTION DESIGN-EDITING-ILLUSTRATION</p>
     <img src="/Images/Contact_Profile_01.png" class="Profile_Image" alt="Profile">
-    <p class="ContactFN" id="About_Section">HANNAH</p>
-    <p class="ContactLN">RICHMAN</p>
+    <p class={`ContactFN${getLightClass("About", "first") ? ` highlight` : ``}`} id="About_Section">HANNAH</p>
+    <p class={`ContactLN${getLightClass("About", "second") ? ` highlight` : ``}`}>RICHMAN</p>
     <img src="/Images/Star_Line.png" class="StarLine" alt="Star Divider">
-    <p class="Description">SCAD graduate with a passion for anything and<br>everything motion and design. From Arizona<br>and based in Nebraska, I love to create pieces<br>that feel inspired and tell stories.</p>
+    <p class={`Description${getLightClass("About", "third") ? ` highlight` : ``}`}>SCAD graduate with a passion for anything and<br>everything motion and design. From Arizona<br>and based in Nebraska, I love to create pieces<br>that feel inspired and tell stories.</p>
     <img src="/Images/Banner.png" class="Banner" alt="Banner">
     <img src="/Images/Moon_Phases.png" class="Moons" alt="Moon Phases">
     <img src="/Images/Battery_v01.png" class="Battery01" alt="Battery">
@@ -129,6 +248,6 @@
     <img src="/Images/Airbag.png" class="Airbag" alt="Airbag Light">
     <div class="XO"><div class="XOX">XOXOXO</div><br><div class="XOXO">OXOXOX</div><br><div class="XOXOX">XOXOXO</div></div>
     <p class="SCAD">SCAD●2024</p>
-    <p class="Email" id="Contact_Section">hannahrichman.art@gmail.com</p>
+    <p class={`Email${getLightClass("Contact", "first") ? ` highlight` : ``}`} id="Contact_Section">hannahrichman.art@gmail.com</p>
     <p class="Copyright">All work © Hannah Richman 2026</p>
 </div>
